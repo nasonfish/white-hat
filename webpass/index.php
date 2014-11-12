b4c39e8177652dd0b2ecb20c977afdce04bbbe5f
Daniel Barnes <nasonfish@nasonfish.com>
2014-11-11 21:42:04 -0700

    use string-only comparison for password in webpass, and trim file_get_contents results

--- webpass/index.php
+++ webpass/index.php
@@ -3,9 +3,9 @@
 </head>
 <body>
   <h2>Welcome!</h2>
-  <p>Source code can be found <a href="https://github.com/nasonfish/white-hat/tree/master/webpass">here</a>!</p>
+  <p>Source code can be found <a href="https://github.com/nasonfish/white-hat/tree/master/webpass" target="_blank">here</a>!</p>
   <hr/>
-  <?php if(file_get_contents("pass.txt") == $_GET["password"]){ ?>
+  <?php if(trim(file_get_contents("pass.txt")) === $_GET["password"]){ ?>
     <p>Welcome to the admin panel for the web application.</p>
     <p>Printing secret data:</p>
     <pre>I hear that computer programming is dangerous. People could get ahold of your passwords.</pre>
